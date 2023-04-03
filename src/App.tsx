73fcc601a571c35ea709fa8fb5d7eb06dade1d2c
Abhijn Chadalawada <abhi@Abhijns-MacBook-Air.local>
2023-04-03 07:54:19 -0700

    Updated packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+// String.prototype.concat = (...strings: string[]) => {
+//   let result = strings[0];
+//   if (length(strings) > 1) {
+    
+//   }
+// }
+
+class TaintedString extends String {
+  tainted: boolean;
+  constructor(value: string) {
+    super(value)
+    this.tainted = true
+  }
+  sanitize() {
+    //TODO: perform sanitizing operations on value
+    console.log(this)
+    return this.toString()
+  }
+}
+
+type TaintableString = string | TaintedString
+
+function isTainted(str: TaintableString): str is TaintedString {
+  return (str as TaintedString).tainted !== undefined;
+}
 
 function App() {
+  const [name, setName] = useState<TaintableString>("");
+  
+  function taintSetName(value: string) {
+    let s = new TaintedString(value);
+    setName(s);
+  }
+
+  function clickHandler() {
+    if (isTainted(name)) {
+      let sanitized = name.sanitize()
+      setName(sanitized);
+    }
+  }
+
+  function evaluator() {
+    console.log(name);
+    if (isTainted(name)) {
+      alert("String is tainted, and must be sanitized before evaluating!");
+    } else {
+      alert(eval(String(name)));
+    }
+  }
+
+  function concatenator() {
+    let a = "hello"
+    let b = "world"
+    let c = a + b;
+    console.log(c)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          onChange={(event) => taintSetName(event.target.value)}
+          id="outlined-basic"
+          label="Input String"
+          variant="outlined"
+        />
+        <Button variant="contained" onClick={clickHandler}>
+          Sanitize
+        </Button>
+        <Button variant="contained" onClick={evaluator}>
+          Evaluate
+        </Button>
+        <Button variant="contained" onClick={concatenator}>
+          Concatenate
+        </Button>
+      </Box>
     </div>
   );
 }
